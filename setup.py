5c4772784d91952e974410866552a05ba359afeb
Petter Nyman <petter@pttr.se>
2014-01-14 11:34:53 +0100

    fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
+from setuptools import find_packages
+
 setup(
   name='arbetsformedlingen',
   version='0.0.1',
   author='Petter Nyman',
   author_email='petter.nyman@willandskill.se',
-  packages=[''],
+  packages=find_packages(),
   install_requires = [
     'requests'
   ],
   url='https://github.com/willandskill/arbetsformedlingen',
-  licence='BSD licence',
+  license='BSD licence',
   description='Wrapper for http://api.arbetsformedlingen.se/',
 )
